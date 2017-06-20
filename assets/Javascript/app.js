37a08ec5f74c1b4a26bdd8d76e8bc63d244baf4c
John Davis <johnsdavis95@gmail.com>
2017-06-19 22:16:06 -0400

    Set up basic profile page

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function() {
-	$("#signUp").on("click", function() {
+
+	$("#entrySignUp").on("click", function() {
+	
 		$(".signUpPage").css("display", "block");
 		
 		$(".entryPage").css("display", "none");
@@ -8,13 +10,39 @@ $(document).ready(function() {
 		// openHomePage();
 	})
 	
-	$("#buttonSignUp").on("click", function() {
-		$(".signUpPage").css("display", "none");
+	// $("#buttonSignUp").on("click", function() {
+	// 	$(".signUpPage").css("display", "block");
 		
-		$(".entryPage").css("display", "block");
+	// 	$(".entryPage").css("display", "none");
 
-	})	
+	// 	var userFirst = $("#firstName").val;
 
+	// 	var userLast = $("#lastName").val;
+
+	// 	// var userEmail = $("#emailAddress").val;
+
+	// 	// var userPass = $("#password").val;
+
+	// 	console.log(userFirst, " + ", userLast);
+
+	// })	
+
+	$( "form" ).on( "submit", function(event) {
+  		event.preventDefault();
+
+  		var userFirst = $("#firstName").val();
+
+		var userLast = $("#lastName").val();
+
+		var userEmail = $("#emailAddress").val();
+
+		var userPass = $("#password").val();
+
+		console.log(userFirst, " + ", userLast);
+
+  		console.log( $( this ).serialize(userFirst) );
+
+	});
 
 	function openHomePage() {
 		
